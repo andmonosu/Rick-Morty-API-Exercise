f4fb5d63428643e572ff2fe48a639ac583e46370
Andreu Montagut Osuna <andmonosu@alum.us.es>
2023-02-20 23:29:48 +0100

    Basic HTML, CSS and Javascript Implementation
    I implemented a basic html layout with some css using grid and flex and after all I have implemented a search engine which obtain all images of a name writed in a input text element.

---
+++ src/js/cardReader.js
@@ -0,0 +1,23 @@
+const searchInput = document.getElementById("search-bar");
+const searchButton = document.getElementById("search-button");
+const content = document.getElementsByClassName("content")[0];
+const processChange = (name) =>{
+    fetch(`https://rickandmortyapi.com/api/character/?name=${name}`)
+        .then(res => res.json())
+        .then(data => createCards(data));
+};
+searchButton.addEventListener("click", ()=>{
+    if(content.hasChildNodes()){
+        while(content.hasChildNodes()){
+            content.removeChild(content.lastChild)
+        }
+    }
+    processChange(searchInput.value)
+});
+const createCards = (apiData) =>{
+    apiData['results'].forEach(data => {
+        const image = document.createElement('img');
+        image.setAttribute('src',data['image']);
+        content.appendChild(image);
+    });
+}
